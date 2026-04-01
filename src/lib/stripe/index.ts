ce66e02a922263736e2f6dfea80b24304790107d
rahmanfaizur <rahman.faizur.fr@gmail.com>
2026-04-02 00:11:36 +0530

    hotfix: stripe SDK instantiation build crash dummy key

--- src/lib/stripe/index.ts
+++ src/lib/stripe/index.ts
@@ -1,6 +1,6 @@
 import Stripe from 'stripe'
 
-export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
+export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY || 'sk_test_placeholder_for_build', {
     apiVersion: '2026-03-25.dahlia',
     typescript: true,
 })
